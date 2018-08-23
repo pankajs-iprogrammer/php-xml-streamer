d3815be4d48af42dd855c8084b87338f0ef04bbe
Pankaj S <pankajs.iprogrammer@gmail.com>
2018-08-23 16:50:08 +0530

    Changed come configuration

--- config/config.php
+++ config/config.php
@@ -6,7 +6,7 @@ ini_set('max_execution_time', 200);
 define('DBHOST', "localhost");
 define('DBNAME', "my_idea_cms");
 define('DBUSER', "root");
-define('DBPASS', "iprogrammer123#");
+define('DBPASS', "XXXXXXX");
 
 // Folder Path
 define('PATH', './docs/');
